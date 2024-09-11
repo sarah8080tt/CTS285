2b7e783421a13e3557af3ce79ba7bba6a69c80a3
housseis7145 <sarah.houssein@icloud.com>
2024-09-11 16:59:21 +0000

    update hello

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -1,18 +1,16 @@
 # minimal Flask app
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     # three quotes for multiline strings
-    return """
-    <h3>Hello from CTS285!!!</h3>
-    <p>This is a paragraph</p>
-    <a href="action">Click here</a>
+    name = "Sarah"
+    return render_template("main_page.html", name=name)
+    
 
-
-    """
+    
 @app.route("/action")
 def action():
     return "Hello from the action route!"
