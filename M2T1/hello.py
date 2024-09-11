1857052ed65149ad04cbf4867fd86f3c2c8a3843
housseis7145 <sarah.houssein@icloud.com>
2024-09-11 12:40:26 -0400

    Update hello.py

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -4,6 +4,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h3>Hello, CTS285!!!</h3>"
-    
+def index():
+    # three quotes for multiline strings
+    return """
+    <h3>Hello from CTS285!!!</h3>
+    <p>This is a paragraph</p>
+    <a href="action">Click here</a>
+
+
+    """
+@app.route("/action")
+def action():
+    return "Hello from the action route!"
