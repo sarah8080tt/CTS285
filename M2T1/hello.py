68ab40698e417d8692bc8b471c5d684ca5ab1907
housseis7145 <sarah.houssein@icloud.com>
2024-09-16 17:04:32 +0000

    68

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -1,16 +1,24 @@
 # minimal Flask app
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
+#print("Starting flask program from ", __name__)
 app = Flask(__name__)
+# Do any app specific setup here
+# for instance, loading a database
+app.config["DEBUG"] = True # will run in debug mode
+# TODO: make this a database or something
+comments = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"]) # we allow posting
 def index():
-    # three quotes for multiline strings
-    #name = "Sarah"
-    return render_template("main_page.html", name=name)
-    
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+    # otherwise, it's a post
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
 
-    
+
+
 @app.route("/action")
 def action():
     return "Hello from the action route!"
